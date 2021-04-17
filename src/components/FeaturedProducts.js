e02abdd0363521aa244abd9ab5ce921f44de8d60
Stelios <mrkarydakis@gmai.com>
2021-04-17 13:12:45 +0300

    initial commit

---
+++ src/components/FeaturedProducts.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useProductsContext } from '../context/products_context'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Error from './Error'
+import Loading from './Loading'
+import Product from './Product'
+
+const FeaturedProducts = () => {
+  const {
+    products_loading: loading,
+    featured_products: featured,
+    products_error: error,
+  } = useProductsContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  if (error) {
+    return <Error />
+  }
+
+  return (
+    <Wrapper className='section'>
+      <div className='title'>
+        <h2>Featured Products</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center featured'>
+        {featured.slice(0, 6).map((product) => {
+          return <Product key={product.id} {...product} />
+        })}
+      </div>
+      <Link to='/products' className='btn'>
+        All Products
+      </Link>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  margin-top: 50vh;
+  background: var(--clr-grey-10);
+  .featured {
+    margin: 4rem auto;
+    display: grid;
+    gap: 2.5rem;
+    img {
+      height: 225px;
+    }
+  }
+  .btn {
+    display: block;
+    width: 148px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  @media (min-width: 576px) {
+    margin-top: 10vh;
+    .featured {
+      grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+    }
+  }
+`
+
+export default FeaturedProducts
